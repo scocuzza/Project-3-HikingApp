f95e4e205a7e06fbbbd185d95cca66b99aff7185
sunitha <sunitha.raghurajan@gmail.com>
2020-11-19 13:40:54 -0500

    added trail tag

--- src/TrailsContainer.js
+++ src/TrailsContainer.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import Trail from './TrailsContainer';
 
 class TrailsContainer extends Component {
-
 	render() {
 		//consditional rendering
 		return (
 			<>
-				<h3>All Trails</h3>
+				<Trail></Trail>
 
 			</>
 		)
