1524e5cb51473beb61070200d234097f7d08a91b
sunitha <sunitha.raghurajan@gmail.com>
2020-11-19 10:42:28 -0500

    added TrailsContainer and Trails

---
+++ src/Trail.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Trail extends Component {
+	render() {
+		return (
+			<>
+				<div >
+					<p>Welcome to Trail xyz</p>
+				</div>
+			</>
+		)
+	}
+
+
+}
+export default Article;
