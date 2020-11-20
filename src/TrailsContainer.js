78f97568e073cb3262dc0d3277bff751f20177d2
Stephen Cocuzza <scocuzza3@gmail.com>
2020-11-19 19:10:48 -0500

    trails container

--- src/TrailsContainer.js
+++ src/TrailsContainer.js
@@ -1,34 +1,41 @@
-
 import React, { Component } from 'react';
 import Trail from './Trail';
+import TrailImage from './TrailImage';
 
-import axios from 'axios';
 
 class TrailContainer extends Component {
-	constructor(props) {
-		super(props)
-
-	}
+	//	constructor(props) {
+	//		super(props)
+	//
+	//	}
 
 
 	render() {
 
 		let allTrails = '';
+		let trailimage = '';
 		if (this.props.trails.length > 0) {
 			allTrails = this.props.trails.map((trail, index) => {
 				return <Trail name={trail.name}
-							  type={trail.type}	
-							  summary={trail.summary}
-							  stars={trail.stars}
-							  starVotes={trail.starVotes}
-							  key={index} />
+					type={trail.type}
+					summary={trail.summary}
+					stars={trail.stars}
+					starVotes={trail.starVotes}
+					key={index} />
+			})
+			trailimage = this.props.trails.map((trail, index) => {
+				return <TrailImage imgMedium={trail.imgMedium}
+
+				/>
 			})
 		}
 
-		return (	
-			<div>
-				<h3>Trails in your Area</h3>
-				{allTrails}
+		return (
+
+
+			<div className="main">
+				<div className="left">{allTrails}</div>
+				<div className="right"><img src={trailimage} /></div>
 			</div>
 		)
 
