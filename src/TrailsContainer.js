726899fff8c5d0801291d3157e8e3b7e2a1dba75
Stephen Cocuzza <scocuzza3@gmail.com>
2020-11-23 09:42:44 -0500

    remove unecessary files

--- src/TrailsContainer.js
+++ src/TrailsContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Trail from './Trail';
-import TrailImage from './TrailImage';
 
 
 class TrailContainer extends Component {
@@ -10,11 +9,6 @@ class TrailContainer extends Component {
 		if (this.props.trails.length > 0) {
 			allTrails = this.props.trails.map((trail, index) => {
 				return <Trail trail={trail}/>
-			})
-			trailimage = this.props.trails.map((trail, index) => {
-				return <TrailImage trail={trail}
-
-				/>
 			})
 		}
 		return (
